78a33e4889c437aef4487ac915698a47c2d28b94
Arpad Borsos <swatinem@swatinem.de>
2024-01-22 17:54:28 +0100

    Add python typing stubs (#19)
    Co-authored-by: Sebastian Zivota <loewenheim@mailbox.org>

---
+++ python/sentry_ophio/enhancers.pyi
@@ -0,0 +1,31 @@
+from typing import Iterator, Any, List
+
+Frame = dict[str, Any]
+
+
+class Cache:
+    def __new__(cls, size: int) -> 'Cache':
+        ...
+
+
+class Enhancements:
+    @staticmethod
+    def empty() -> 'Enhancements':
+        ...
+
+    def __new__(cls, input: str, cache: Cache) -> 'Enhancements':
+        ...
+
+    @staticmethod
+    def from_config_structure(input: bytes, cache: Cache) -> 'Enhancements':
+        ...
+
+    def extend_from(self, other: 'Enhancements'):
+        ...
+
+    def apply_modifications_to_frames(
+        self,
+        frames: Iterator[Frame],
+        exception_data: dict[str, Any],
+    ) -> List[Frame]:
+        ...
