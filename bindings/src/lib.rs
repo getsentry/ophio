b71c734116b90fa8ba0fac119a319812a85f2ff5
Arpad Borsos <arpad.borsos@sentry.io>
2024-01-24 13:07:02 +0100

    Rework python package/module exporting
    I believe I understand the problem with packages/modules now.
    
    A python *package* is a directory with a `__init__.py`, and a *module* is just a python file.
    
    When writing `import`s in Python, the *parent* of that path *must* be a package.
    
    So `from this_is_a_package.this_is_a_module import *` works.
    Whereas `from this_is_a_parent_module.this_is_a_child_module import *` does not.
    
    However it would seem that one limitation of python extension modules is that they are in fact *modules*, and not *packages*.
    So you cannot *directly* import stuff from their sub-modules.
    
    Long story short, the workaround I found online was manipulating the global `sys.modules` directory to make this work, however that messes with tools like `Pylance` in VS Code.
    
    Instead, I now just export a flat list of classes (and maybe functions in the future) from the `_bindings`, and use python code to reexport things as targeted sub-modules.

--- bindings/src/lib.rs
+++ bindings/src/lib.rs
@@ -3,34 +3,12 @@ use pyo3::prelude::*;
 mod enhancers;
 mod proguard;
 
-macro_rules! add_module {
-    ($py:ident : $parent:ident . $name:ident) => {
-        // This uses the workaround from:
-        // https://github.com/PyO3/pyo3/issues/759#issuecomment-1811992321
-        // For the problem raised in https://pyo3.rs/v0.20.0/module#python-submodules
-        {
-            let module = PyModule::new($py, stringify!($name))?;
-            $parent.add_submodule(module)?;
-            let qualified_name = concat!("sentry_ophio.", stringify!($name));
-            $py.import("sys")?
-                .getattr("modules")?
-                .set_item(qualified_name, module)?;
-            module.setattr("__name__", qualified_name)?;
-            module
-        }
-    };
-}
-
-/// A Python module implemented in Rust.
 #[pymodule]
-fn sentry_ophio(py: Python, m: &PyModule) -> PyResult<()> {
-    let proguard_module = add_module!(py: m.proguard);
-    proguard_module.add_class::<proguard::JavaStackFrame>()?;
-    proguard_module.add_class::<proguard::ProguardMapper>()?;
-
-    let enhancers_module = add_module!(py: m.enhancers);
-    enhancers_module.add_class::<enhancers::Enhancements>()?;
-    enhancers_module.add_class::<enhancers::Cache>()?;
+fn _bindings(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<proguard::JavaStackFrame>()?;
+    m.add_class::<proguard::ProguardMapper>()?;
+    m.add_class::<enhancers::Enhancements>()?;
+    m.add_class::<enhancers::Cache>()?;
 
     Ok(())
 }
