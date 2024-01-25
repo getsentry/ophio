23b8df3f2458a33e41b8ab77326645208be8b010
Arpad Borsos <arpad.borsos@sentry.io>
2024-01-25 09:36:30 +0100

    override the module

--- python/sentry_ophio/enhancers.py
+++ python/sentry_ophio/enhancers.py
@@ -1,1 +1,4 @@
 from ._bindings import Cache, Enhancements
+
+Cache.__module__ = __name__
+Enhancements.__module__ = __name__
--- python/sentry_ophio/proguard.py
+++ python/sentry_ophio/proguard.py
@@ -1,1 +1,4 @@
 from ._bindings import JavaStackFrame, ProguardMapper
+
+JavaStackFrame.__module__ = __name__
+ProguardMapper.__module__ = __name__
