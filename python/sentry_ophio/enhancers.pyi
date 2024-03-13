8914ea7fcde40ea729156f1037a60201bea7342e
Sebastian Zivota <loewenheim@mailbox.org>
2024-03-13 14:46:52 +0100

    fix(bindings): Add type info for Component.__new__

--- python/sentry_ophio/enhancers.pyi
+++ python/sentry_ophio/enhancers.pyi
@@ -4,16 +4,23 @@ ExceptionData = dict[str, bytes | None]
 Frame = dict[str, Any]
 ModificationResult = tuple[str | None, bool | None]
 
+
 class Component:
     contributes: bool | None
     is_prefix_frame: bool
     is_sentinel_frame: bool
     hint: str | None
 
+    def __new__(
+        cls, is_prefix_frame: bool, is_sentinel_frame: bool, contributes: bool | None
+    ): ...
+
+
 class AssembleResult:
     contributes: bool
     hint: str | None
     invert_stacktrace: bool
+
 
 class Cache:
     """
@@ -23,6 +30,7 @@ class Cache:
     """
 
     def __new__(cls, size: int) -> Cache: ...
+
 
 class Enhancements:
     """
@@ -34,6 +42,7 @@ class Enhancements:
         """
         Creates an Enhancements object with no rules.
         """
+
     @staticmethod
     def parse(input: str, cache: Cache) -> Enhancements:
         """
@@ -42,6 +51,7 @@ class Enhancements:
         :param input: The input string.
         :param cache: A cache that memoizes rule and regex construction.
         """
+
     @staticmethod
     def from_config_structure(input: bytes, cache: Cache) -> Enhancements:
         """
@@ -50,10 +60,12 @@ class Enhancements:
         :param input: The input in msgpack format.
         :param cache: A cache that memoizes rule and regex construction.
         """
+
     def extend_from(self, other: Enhancements):
         """
         Adds all rules from the other Enhancements object to this one.
         """
+
     def apply_modifications_to_frames(
         self,
         frames: list[Frame],
@@ -69,6 +81,7 @@ class Enhancements:
         :param exception_data: Exception data to match against rules. Supported
                                fields are "ty", "value", and "mechanism".
         """
+
     def assemble_stacktrace_component(
         self,
         frames: list[Frame],
